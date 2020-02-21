e88f77cd5a3858ecb05919929370b8880d3fc5dd
Bohdan <bohdanimiela@gmail.com>
2020-02-21 11:37:32 +0100

    Simplify request object

--- src/components/location/decodeLocation.ts
+++ src/components/location/decodeLocation.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'https://wtw-api.herokuapp.com/locations';
 
 export default (lat: string, long: string) => {
-  return axios.post(API_URL, { coords: { lat: lat, long:long } }).then(response => {
+  return axios.post(API_URL, { lat: lat, long:long }).then(response => {
     return response.data.location! as string
   });
 };
