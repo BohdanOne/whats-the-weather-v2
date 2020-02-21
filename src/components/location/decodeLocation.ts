eec8588f413bf5435c4590717f9c2fa9212d80c4
Bohdan <bohdanimiela@gmail.com>
2020-02-21 11:06:17 +0100

    Fetch decoded location name via app own server

--- src/components/location/decodeLocation.ts
+++ src/components/location/decodeLocation.ts
@@ -1,20 +1,9 @@
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_OPENCAGE_API_KEY;
-const BASE_URL = 'https://api.opencagedata.com/geocode/v1/';
-
-type GeocodeResponse = { results: {components: {town?: string; city?: string}}[]}
+const API_URL = 'http://localhost:8080/locations';
 
 export default (lat: string, long: string) => {
-  const query = encodeURI(`${lat},${long}`);
-  return axios.get(`${BASE_URL}/json?q=${query}&key=${API_KEY}`)
-    .then(response => response.data)
-    .then((data: GeocodeResponse) => {
-      if (data.results[0].components.town) {
-        return data.results[0].components.town;
-      } else {
-        return data.results[0].components.city;
-      }
-    })
-    .catch(e => console.log(e));
+  return axios.post(API_URL, { coords: { lat: lat, long:long } }).then(response => {
+    return response.data.location! as string
+  });
 };
