a534c390b1d229506fe08e9a064cabce828b7080
Bohdan <bohdanimiela@gmail.com>
2020-02-25 12:18:24 +0100

    Updates

---
+++ precache-manifest.ef5c4589e5c9b70c95025ec38e39de18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a84e8bbddc3b40a546b0f311a31f92",
+    "url": "/whats-the-weather-v2/index.html"
+  },
+  {
+    "revision": "cadc8a2ccf758041bb18",
+    "url": "/whats-the-weather-v2/static/css/main.0007f071.chunk.css"
+  },
+  {
+    "revision": "1e155d46393cedebcce6",
+    "url": "/whats-the-weather-v2/static/js/2.fb227d88.chunk.js"
+  },
+  {
+    "revision": "70a379e400fd46d34773",
+    "url": "/whats-the-weather-v2/static/js/3.ad167cdd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/whats-the-weather-v2/static/js/3.ad167cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4839cd510efb0b00b1",
+    "url": "/whats-the-weather-v2/static/js/4.b468b4a6.chunk.js"
+  },
+  {
+    "revision": "cb084e8b6f2d04f1b1ce",
+    "url": "/whats-the-weather-v2/static/js/5.b3967b72.chunk.js"
+  },
+  {
+    "revision": "6d46c7b4a5a6d12add4a",
+    "url": "/whats-the-weather-v2/static/js/6.66a4b022.chunk.js"
+  },
+  {
+    "revision": "cadc8a2ccf758041bb18",
+    "url": "/whats-the-weather-v2/static/js/main.51f404e3.chunk.js"
+  },
+  {
+    "revision": "b052e4f4e60632bbc9bb",
+    "url": "/whats-the-weather-v2/static/js/runtime-main.dcd8d3e6.js"
+  }
+]);
---
+++ static/js/4.b468b4a6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpwtw-v2"]=this["webpackJsonpwtw-v2"]||[]).push([[4],{35:function(e,t,n){"use strict";var a=n(0),l=n.n(a);t.a=function(e){var t=e.title;return l.a.createElement("header",null,l.a.createElement("h1",null,t))}},66:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),r=n(35);t.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(r.a,{title:"about"}),l.a.createElement("p",null,"Page not ready yet..."),l.a.createElement("p",null,"Come back soon!"))}}}]);
+//# sourceMappingURL=4.b468b4a6.chunk.js.map
